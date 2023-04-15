1981ef4a72b190f5edd35b0e43801a4e428d5243
Manish <manish.sharma22221@gmail.com>
2023-04-15 22:03:42 +0530

    northernHill part-2 first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("Its working");
+
+let menu= document.querySelector('#menu-bar');
+let navbar= document.querySelector('.navbar');
+let vidBtn= document.querySelectorAll('.vid-btn');
+
+
+
+
+
+
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', ()=>{
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    
+});
+
+vidBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+
+        let src= btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    })
+})
